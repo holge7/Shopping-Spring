8bc2783c90c23bcb2e89c6be02cc47c591e13103
holge7 <munoz.jorge.dev@gmail.com>
2023-02-22 18:05:58 +0100

    delete doc-service and create manteiner-service

---
+++ shopping/manteiner-service/src/main/java/holge/shopping/manteinerservice/service/DocServiceImpl.java
@@ -0,0 +1,41 @@
+package holge.shopping.manteinerservice.service;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.springdoc.core.properties.AbstractSwaggerUiConfigProperties.SwaggerUrl;
+
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.stereotype.Service;
+
+import commons.dto.ApiResponse;
+
+@Service
+public class DocServiceImpl {
+	
+	private DiscoveryClient discoveryClient;
+	
+	public DocServiceImpl(DiscoveryClient discoveryClient) {
+		this.discoveryClient = discoveryClient;
+	}
+	
+	public ApiResponse getSwaggerURLs() {
+		List<SwaggerUrl> urls = new LinkedList<>();
+		
+		List<String> blackListDoc = Arrays.asList("api-gateway");
+		
+		List<String> goodService = discoveryClient.getServices();
+		
+		goodService.stream()
+			.filter(service -> !blackListDoc.contains(service))
+			.forEach(serviceName -> 
+				discoveryClient.getInstances(serviceName).forEach(ServiceIntance -> 
+				urls.add(new SwaggerUrl(serviceName, ServiceIntance.getUri()+"/swagger-ui.html", serviceName)))
+			);
+		
+		return new ApiResponse(false, "", Map.of("urls", urls));		
+	}
+
+}
